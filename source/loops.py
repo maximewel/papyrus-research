47d316b97d8472d353901111e05dfe49d6a1ec44
Maxime Welcklen <max.geminiani@gmail.com>
2024-11-09 13:07:59 +0100

    fix(transfo_training_loop): In case of not normalized, round outputs

--- source/loops.py
+++ source/loops.py
@@ -128,6 +128,8 @@ def do_training(model: HwTransformer, train_loader: DataLoader, test_loader: Dat
 
                     # Iterate over the sequences untill all are over. 
                     y_pred = model.forward(images_patches, masks, sequences)
+                    if not normalized_sequences:
+                        y_pred = torch.round(y_pred)
 
                     ### COORD loss ###
                     if LogChannels.LOSS_DETAILED in logger.channels:
@@ -184,7 +186,9 @@ def do_training(model: HwTransformer, train_loader: DataLoader, test_loader: Dat
                     for batch in test_loader:
                         original_images, images_patches, masks, sequences, labels = data_from_batch(batch, device)
 
-                        y_pred = model.forward(images_patches, masks, sequences)
+                        y_pred = model.forward(images_patches, masks, sequences)                    
+                        if not normalized_sequences:
+                            y_pred = torch.round(y_pred)
 
                         ### COORD loss ###
                         coord_loss = coord_criterion(y_pred, labels)
